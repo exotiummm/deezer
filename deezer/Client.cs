5e147b3c9b54daf2630b8c47716f2da1b6f6591a
vlasov timofey <connorflaggy@gmail.com>
2026-06-28 15:44:02 +0300

    added artist search

---
+++ deezer/Client.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace deezer
+{
+    internal class Client
+    {
+        private readonly HttpClient _httpClient = new HttpClient();
+
+        public async Task<string> SearchArtistAsync(string query)
+        {
+            string url = $"https://api.deezer.com/search/artist?q={query}";
+            var response = await _httpClient.GetAsync(url);
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
